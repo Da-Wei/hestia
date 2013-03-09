d0bfdddb26600968edd13b6983657c38894d4b57
Dawei <dawei@fakefor.nospam>
2013-03-10 00:09:27 +0100

    Making menu work

--- controller/src/sketch.ino
+++ controller/src/sketch.ino
@@ -3,29 +3,35 @@
 unsigned long delayCounter;
 
 Inverter inverter = Inverter();
+InverterSensors sensors;
 
 void displayMenu() {
-  Serial.println('Hestia');
-  Serial.println('======');
-  Serial.println('1. Status');
-  Serial.println('2. Switch Inverter On/Off');
+  Serial.println("Hestia");
+  Serial.println("======");
+  Serial.println("1. Status");
+  Serial.println("2. Switch Inverter On/Off");
+  Serial.println("");
+  Serial.print("==> ");
 }
 
 void setup()
 {
+  Serial.begin(9600);
   delayCounter = millis();
   inverter.setSlaveID(2);
 }
 
 void loop()
 {
-  InverterSensors sensors;
   char cmd;
 
   if ( ( millis() - delayCounter ) >= 10000 ) {
     // Request inverter for values
-    inverter.get(&sensors);
-
+    if(inverter.get(&sensors)) 
+      {
+	Serial.println("Get sensors...");
+      }
+    
     // Store values with timestamp
     // tstamp = getTimestamp();
     // storeData(tstamp, sensors);
@@ -40,8 +46,14 @@ void loop()
   if ( Serial.available() > 0 ){
     cmd = Serial.read();
     switch(cmd) {
-    case '\n':
+    case '\r':
+    case 'h':
       displayMenu();
+      break;
+    case '1':
+      Serial.print("Water Output Temperature : ");
+      Serial.println(sensors.waterOutputTemp);
+      break;
     }
   }
 }
